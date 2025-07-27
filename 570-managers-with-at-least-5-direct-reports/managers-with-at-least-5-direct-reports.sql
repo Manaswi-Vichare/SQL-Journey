a94b50cc6e5bb1386d6b28e73d90cdb356c69872
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-07-28 02:45:34 +0530

    Time: 321 ms (85.52%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,44 @@
+# Write your MySQL query statement below
+#sqljourney-MV 
+SELECT name FROM Employee
+WHERE id IN(
+    SELECT managerId FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(DISTINCT id) >= 5
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT name FROM Employee
+-- WHERE id IN (
+--     SELECT managerId FROM Employee 
+--     GROUP BY managerId
+--     HAVING COUNT(DISTINCT id) >= 5
+-- )
