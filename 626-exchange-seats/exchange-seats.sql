b672460b8ebc3335a816cd840f0d7f5c7568fb3f
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-07 21:53:31 +0530

    Time: 359 ms (35.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+#sqljourney-MV
+SELECT
+CASE WHEN id%2 = 0 THEN id - 1
+     WHEN id%2 != 0 AND id = (SELECT MAX(id) FROM Seat) THEN id
+     WHEN id%2 != 0 THEN id + 1  
+     END AS id, 
+     student 
+FROM Seat
+ORDER BY id
