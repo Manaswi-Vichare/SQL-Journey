9431dfb9f3c92925959fecfcd58c3be7926e7707
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-12 01:02:03 +0530

    Time: 789 ms (98.97%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+#sqljourney-MV
+SELECT Department, Employee, Salary
+FROM (
+    SELECT 
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        DENSE_RANK() OVER (PARTITION BY d.name ORDER BY Salary DESC) AS rank_1
+    FROM Employee e
+    JOIN Department d
+    ON e.departmentId = d.id
+) AS rank_table
+WHERE rank_1 <= 3;
