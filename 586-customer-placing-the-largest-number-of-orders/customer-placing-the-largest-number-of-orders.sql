9eb82293e3269ca2f787fe0912558b9002714f61
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2026-05-26 15:15:22 +0900

    Time: 490 ms (33.46%) | Memory: 0B (100.00%) - LeetSync

---
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+#sqljourney-mv
+SELECT MAX(customer_number) as customer_number FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(customer_number) DESC
+LIMIT 1
