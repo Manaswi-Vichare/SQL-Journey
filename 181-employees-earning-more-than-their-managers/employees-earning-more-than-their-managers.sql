d8624318bff74ad321416beae1c3d77f13708765
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2026-05-26 10:41:48 +0900

    Time: 380 ms (62.19%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 #sqljourney-MV
-SELECT e2.name as Employee FROM employee e1
-INNER JOIN employee e2
-ON e1.id = e2.managerId
-WHERE e1.salary < e2.salary
+SELECT e2.name as Employee FROM Employee e1
+INNER JOIN Employee e2
+ON e1.id = e2.managerID
+WHERE e2.salary > e1.salary
