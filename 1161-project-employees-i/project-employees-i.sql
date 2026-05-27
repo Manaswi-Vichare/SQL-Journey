db35d29aec58586a25d65e242b0791147d02cc9e
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2026-05-27 09:42:58 +0900

    Time: 501 ms (74.79%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,11 +1,8 @@
 # Write your MySQL query statement below
-#sqljourney-MV
-SELECT 
-    p.project_id,
-    ROUND(
-        SUM(e.experience_years) / COUNT(e.employee_id)
-    , 2)
-    as average_years
-FROM Project p
-JOIN Employee e ON p.employee_id = e.employee_id
-GROUP by p.project_id
+#sqljourney-mv
+SELECT P.project_id, 
+       ROUND((SUM(E.experience_years) / COUNT(P.project_id)), 2) AS average_years 
+FROM Project P
+LEFT JOIN Employee E
+ON E.employee_id = P.Employee_id
+GROUP BY P.project_id
