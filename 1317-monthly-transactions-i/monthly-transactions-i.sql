9a89eb72cab3b728a1e55f3dbd32a4429e4bbbb2
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2026-05-28 16:28:05 +0900

    Time: 657 ms (20.32%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,12 +1,16 @@
 # Write your MySQL query statement below
-#sqljourney-MV
-SELECT
-    DATE_FORMAT(trans_date, '%Y-%m') as month,
-    country,
-    COUNT(*) as trans_count,
-    COUNT(IF(state = 'approved', 1, Null)) as approved_count,
-    SUM(amount) as trans_total_amount,
-    SUM(IF(state = 'approved', amount, 0))  as approved_total_amount
+#sqljourney-mv
+SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month,
+       country,
+       COUNT(id) AS trans_count,
+       SUM(IF(state='approved', 1, 0)) AS approved_count,
+       SUM(amount) AS trans_total_amount,
+       SUM(IF(state='approved', amount, 0)) AS approved_total_amount
 FROM transactions
 GROUP BY month, country
-ORDER BY month, country;
+       
+
+
+
+
+
