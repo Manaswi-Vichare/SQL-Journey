73c75a54b339afbaf8c6674ab949ef69fca41d65
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2026-05-28 09:51:05 +0900

    Time: 1507 ms (64.71%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
-#sqljourey-MV
+#sqljourney-mv
 SELECT 'Low Salary' AS category,
         COUNT(IF(income < 20000, 1, NULL)) AS accounts_count
 FROM Accounts
 UNION ALL
-SELECT 'Average Salary',
+SELECT 'Average Salary' AS category,
         COUNT(IF(income >= 20000 AND income <= 50000, 1, NULL))
-FROM Accounts     
+FROM Accounts
 UNION ALL
-SELECT 'High Salary',
+SELECT 'High Salary' AS category,
         COUNT(IF(income > 50000, 1, NULL))
 FROM Accounts
-order by accounts_count desc
+GROUP BY category
