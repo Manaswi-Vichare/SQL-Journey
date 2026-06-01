11072f839ad043079c55e2a3649d3ac8cacd88f7
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2026-06-01 13:14:16 +0900

    Time: 493 ms (48.09%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,13 +1,10 @@
 # Write your MySQL query statement below
-#sqljourney-mv
 SELECT id,
-       CASE
-            WHEN p_id IS NULL THEN "Root"
-            WHEN id NOT IN (
-                SELECT p_id FROM Tree
-                WHERE p_id IS NOT NULL
-            )
-            THEN 'Leaf' ELSE 'Inner'
-        END
-       AS type
-FROM Tree 
+    CASE 
+       WHEN p_id IS NULL THEN 'Root'
+       WHEN id IN(
+        SELECT p_id FROM Tree
+       ) THEN 'Inner'
+       ELSE 'Leaf' END
+    AS type
+FROM Tree
