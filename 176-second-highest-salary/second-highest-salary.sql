b60b13d1a97ac7b6a10f544acdc46b9a73d57162
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2026-06-01 14:08:08 +0900

    Time: 301 ms (24.19%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
-#sqljourney-MV
 SELECT (
-  SELECT DISTINCT salary
-  FROM Employee
-  ORDER BY salary DESC
-  LIMIT 1 OFFSET 1
+    SELECT DISTINCT salary FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1
+    OFFSET 1
 ) AS SecondHighestSalary
